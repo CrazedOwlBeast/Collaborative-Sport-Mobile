856b0a831ba6aeabef1a502782f502cb81f712aa
CrazedOwlBeast <nicolasmorita15@gmail.com>
2023-03-07 11:27:38 -0500

    Local database work in progress

--- lib/workout_info.dart
+++ lib/workout_info.dart
@@ -1,30 +1,63 @@
+import 'dart:convert';
+
+final String tableWorkouts = 'workouts';
+
+class WorkoutFields {
+  static final String id = '_id';
+  static final String name = 'name';
+  static final String jsonString = 'jsonString';
+}
+
 class Workout {
+  final int id;
+  final String name;
+  final String jsonString;
 
-  final String type;
-  final List<String> partners;
-  final double duration;
-  final double distance;
-  /*
-  List<int>? hr;
-  List<int>? speed;
-  */
-
-  Workout({
-    required this.type,
-    required this.duration,
-    required this.distance,
-    required this.partners,
-    /*
-    this.hr,
-    this.speed
-     */
+  const Workout({
+    required this.id,
+    required this.name,
+    required this.jsonString,
   });
 
-  Map<String, dynamic> toMap() {
+  // final String type;
+  // final List<String> partners;
+  // final double duration;
+  // final double distance;
+  // /*
+  // List<int>? hr;
+  // List<int>? speed;
+  // */
+  //
+  // Workout({
+  //   required this.type,
+  //   required this.duration,
+  //   required this.distance,
+  //   required this.partners,
+  //   /*
+  //   this.hr,
+  //   this.speed
+  //    */
+  // });
+
+  Workout copy({
+    int? id,
+    String? name,
+    String? jsonString,
+  }) => Workout(
+      id: id ?? this.id,
+      name: name ?? this.name,
+      jsonString: jsonString ?? this.jsonString
+  );
+
+  // static Workout fromJson(Map<String, Object?> json) {
+  //
+  // }
+
+  Map<String, Object?> toJson() {
     return {
-      'type': type,
-      'duration': duration,
-      'distance': distance,
+      'id': id,
+      'name': name,
+      'jsonString': jsonString,
     };
   }
 
